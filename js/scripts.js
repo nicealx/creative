c6a7d007033d37db5bced93fcf7dc359e6740c7a
Alexey <kav.prok@gmail.com>
2019-03-08 22:31:10 +0700

    Сверстал подвал, подключил гугл картуб но пока она не работает

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ $('.header-bottom-scroll').click(function(){
   $('html, body').animate({ scrollTop: $('.feature').offset().top }, 500);
 });
 
+
+function initMap() {
+  var coordinates = {lat: 53.355062, lng: 83.769559},
+  
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: coordinates
+      });
+}
