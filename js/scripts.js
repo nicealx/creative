2cd72bd10b9479a6ad017d6658534f817e606275
Alexey <kav.prok@gmail.com>
2019-04-16 20:35:30 +0700

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 //   $('html, body').animate({ scrollTop: $('.feature').offset().top }, 500);
 // });
 
-const scrollArrows = document.querySelectorAll('a[href*="#"]');
+const scrollArrows = document.querySelectorAll('a[href="#"]');
 
 for(let scrollArrow of scrollArrows){
     scrollArrow.addEventListener('click', (e) => {
